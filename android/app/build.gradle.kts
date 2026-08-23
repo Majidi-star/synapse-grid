f374b3356a1c77d02f99006c4d707e069c7ec16c
Amirhosein Majidi <tsp10majidi@gmail.com>
2026-08-23 16:36:24 +0330

    Set Android compileSdk to 36 to satisfy AAR metadata check requirements

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.synapsegrid.recall_app"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
